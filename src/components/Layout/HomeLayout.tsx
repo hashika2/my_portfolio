5c459edba0b02daee4a2cebea12e5b105a17da77
anjana6 <anjanashakthi95@gmail.com>
2021-10-25 23:24:52 +0530

    identify the screen size dimentions

--- src/components/Layout/HomeLayout.tsx
+++ src/components/Layout/HomeLayout.tsx
@@ -1,15 +1,46 @@
-import React, { FC } from 'react'
+import React, { FC,useState,useEffect } from 'react';
+import {
+    MenuOutlined
+  } from '@ant-design/icons';
+  import styles from './layout.module.css';
+import LeftNavBar from '../navbar/LeftNavBar';
+
+
+
 
 const HomeLayout: FC = ({children}) :JSX.Element => {
+    
+    const hasWindow = typeof window !== 'undefined';
+    const getWindowDimensions = () => {
+        const width = hasWindow ? window.innerWidth : null;
+        return width;
+      }
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  
+  console.log('***************',windowDimensions)
+
     return (
+        <>
+        <div className={styles.menuIcon}><MenuOutlined /></div>
         <div className="grid grid-cols-5">
-            <div>
-                sdfsfs
+            <div className='hidden xl:block'>
+                <LeftNavBar/>
             </div>
-            <div className='col-span-4'>
+            <div className='col-span-5 xl:col-span-4'>
                 {children}
             </div>
         </div>
+        </>
     )
 }
 
--- src/components/navbar/LeftNavBar.tsx
+++ src/components/navbar/LeftNavBar.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from 'react';
-import { Menu, Button } from 'antd';
+import React, { FC,useState } from 'react';
+import { Menu, Button,Drawer } from 'antd';
 import {
     PieChartOutlined,
     DesktopOutlined
@@ -7,10 +7,24 @@ import {
 
 
 const LeftNavBar:FC = () :JSX.Element => {
+    const [visible, setVisible] = useState(false);
+  const showDrawer = () => {
+    setVisible(true);
+  };
+  const onClose = () => {
+    setVisible(false);
+  };
     return (
         
         <div>
-        <Button type="primary">Primary Button</Button>
+            <Button type="primary" onClick={showDrawer}>
+        Open
+      </Button>
+        <Drawer title="Basic Drawer" placement="left" onClose={onClose} visible={visible}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
         </div>
     )
 }
