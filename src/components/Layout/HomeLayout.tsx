d4ae1092c306ba7047ee9282be93f19d070f7f5b
anjana6 <anjanashakthi95@gmail.com>
2021-10-26 12:08:49 +0530

    create responsive navebar layer

--- src/components/Layout/HomeLayout.tsx
+++ src/components/Layout/HomeLayout.tsx
@@ -10,33 +10,33 @@ import LeftNavBar from '../navbar/LeftNavBar';
 
 const HomeLayout: FC = ({children}) :JSX.Element => {
     
-    const hasWindow = typeof window !== 'undefined';
-    const getWindowDimensions = () => {
-        const width = hasWindow ? window.innerWidth : null;
-        return width;
-      }
-    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  //   const hasWindow = typeof window !== 'undefined';
+  //   const getWindowDimensions = () => {
+  //       const width = hasWindow ? window.innerWidth : null;
+  //       return width;
+  //     }
+  //   const [windowDimensions, setWindowDimensions] = useState <number|null>(getWindowDimensions());
 
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
+  // useEffect(() => {
+  //   function handleResize() {
+  //     setWindowDimensions(getWindowDimensions());
+  //   }
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+  //   window.addEventListener('resize', handleResize);
+  //   return () => window.removeEventListener('resize', handleResize);
+  // }, []);
 
   
-  console.log('***************',windowDimensions)
+  // console.log('***************',windowDimensions)
 
     return (
         <>
-        <div className={styles.menuIcon}><MenuOutlined /></div>
+        {/* <div className={styles.menuIcon}><MenuOutlined /></div> */}
         <div className="grid grid-cols-5">
-            <div className='hidden xl:block'>
+            <div>
                 <LeftNavBar/>
             </div>
-            <div className='col-span-5 xl:col-span-4'>
+            <div className='col-span-5 lg:col-span-4'>
                 {children}
             </div>
         </div>
--- src/components/navbar/LeftNavBar.tsx
+++ src/components/navbar/LeftNavBar.tsx
@@ -1,30 +1,42 @@
-import React, { FC,useState } from 'react';
+import React, { FC,useState,useEffect } from 'react';
 import { Menu, Button,Drawer } from 'antd';
-import {
-    PieChartOutlined,
-    DesktopOutlined
-  } from '@ant-design/icons';
+import {MenuOutlined} from '@ant-design/icons';
+import styles from './styles.module.css';
+
+const getWindowDimensions = () : boolean => {
+  const hasWindow = typeof window !== 'undefined';
+  const width = hasWindow ? window.innerWidth : null;
+  if(width && width < 1024){
+    return false;
+  }
+  return true;
+}
 
 
 const LeftNavBar:FC = () :JSX.Element => {
-    const [visible, setVisible] = useState(false);
+    const [visible, setVisible] = useState <boolean>(getWindowDimensions());
+    // const [windowDimensions, setWindowDimensions] = useState <number|null>(getWindowDimensions());
+
+  useEffect(() => {
+    const handleResize = () => {
+      
+      setVisible(getWindowDimensions())
+    }
+    
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   const showDrawer = () => {
-    setVisible(true);
+    setVisible(!visible);
   };
   const onClose = () => {
     setVisible(false);
   };
     return (
         
-        <div>
-            <Button type="primary" onClick={showDrawer}>
-        Open
-      </Button>
-        <Drawer title="Basic Drawer" placement="left" onClose={onClose} visible={visible}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-      </Drawer>
+        <div className={visible? styles.sideBarActive :styles.sideBar}>
+        <span className={`lg:invisible ${styles.toggleBtn}`} onClick={showDrawer}><MenuOutlined/></span>
         </div>
     )
 }
