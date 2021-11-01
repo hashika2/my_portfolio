0609580841fc8d9a56c0d5b745ca4fc66458c7e0
anjana6 <anjanashakthi95@gmail.com>
2021-11-01 22:54:04 +0530

    coludinary url to skills

--- src/components/skill/SkillItem.tsx
+++ src/components/skill/SkillItem.tsx
@@ -4,16 +4,17 @@ import styles from './styles.module.css';
 
 interface ISkillItem {
     skill: string
+    imageUrl: string
 }
 
-const SkillItem :FC<ISkillItem> = ({skill}) :JSX.Element => {
+const SkillItem :FC<ISkillItem> = ({skill,imageUrl}) :JSX.Element => {
     return (
         <div className='my-4 px-3'>
             <div className='font2xl flex justify-between items-center'>
                 <div>{skill}</div>
                 <div className={`bg-green-500 rounded-full ${styles.framworkIcon}`}>
-                    <Image alt='profile' width={60} preview={false} style={{borderRadius:"50%"}}
-                        src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" />
+                    <Image alt='profile' width={65} preview={false} style={{borderRadius:"50%"}}
+                        src={imageUrl} />
                 </div>
             </div>
             {/* <div className='space-x-4'>
--- src/utils/commenDetails.ts
+++ src/utils/commenDetails.ts
@@ -27,11 +27,27 @@ export const experienceDetails = [
 
 
 export const skillDetails = [
-    { skill1 :'Javascript', skill2:'Typescript'}, 
-    {skill1: 'ReactJs', skill2: 'NextJs'}, 
-    {skill1: 'NodeJs(Expressjs)',skill2: 'NestJs'},
-    {skill1: 'tailwindcss',skill2: 'serverless'},
-    {skill1: 'MongoDB',skill2: 'MySQL'}
+    { 
+        skill1 :'Javascript', skill1Url: 'https://res.cloudinary.com/ashakthi/image/upload/v1635785755/samples/protfolio/javascript_wyku8h.png', 
+        skill2:'Typescript', skill2Url: 'https://res.cloudinary.com/ashakthi/image/upload/v1635785711/samples/protfolio/typescript_yon21g.png'
+    }, 
+    {
+        skill1: 'ReactJs', skill1Url: 'https://res.cloudinary.com/ashakthi/image/upload/v1635785726/samples/protfolio/react_rcfnhv.png' ,
+        skill2: 'NextJs' ,skill2Url: 'https://res.cloudinary.com/ashakthi/image/upload/v1635787283/samples/protfolio/next1_p6e4ys.png'
+    },
+    {
+
+        skill1: 'NodeJs(Expressjs)', skill1Url: 'https://res.cloudinary.com/ashakthi/image/upload/v1635787066/samples/protfolio/node_lkrbjw.png',
+        skill2: 'NestJs' ,skill2Url: 'https://res.cloudinary.com/ashakthi/image/upload/v1635785719/samples/protfolio/nestjs_u1wiom.png'
+    },
+    {
+        skill1: 'tailwindcss', skill1Url: 'https://res.cloudinary.com/ashakthi/image/upload/v1635787069/samples/protfolio/taildwind_nr3eaq.png',
+        skill2: 'serverless', skill2Url: 'https://res.cloudinary.com/ashakthi/image/upload/v1635787071/samples/protfolio/serverless_imnmht.png'
+    },
+    {
+        skill1: 'MongoDB',skill1Url: 'https://res.cloudinary.com/ashakthi/image/upload/v1635785736/samples/protfolio/mongodb_oxjjbn.jpg', 
+        skill2: 'MySQL', skill2Url: 'https://res.cloudinary.com/ashakthi/image/upload/v1635785745/samples/protfolio/mysql_ib9wje.png'
+    }
 ]
 
 
