ff8c85b8e2ea21fe082a97280086ca7fe8ad54d6
anjana6 <anjanashakthi95@gmail.com>
2021-11-02 16:46:49 +0530

    change scrollbar color

--- src/utils/commenDetails.ts
+++ src/utils/commenDetails.ts
@@ -1,5 +1,5 @@
 export const educationDetails = [
-    {duration: '2018 - 2021',degree:'B.Sc (Hons.) Software Engineering', place: 'University of Kelaniya', result: 'Web Application developemt & Mobile Application development'},
+    {duration: '2018 - 2022',degree:'B.Sc (Hons.) Software Engineering', place: 'University of Kelaniya', result: 'Web Application developemt & Mobile Application development'},
     {duration: '2012 - 2016',degree:'GCE Advanced Level', place: "ST.Thomas' Boys' College Matara", result: 'Maths-A Chemisty-B physics-B'},
     {duration: '2006 - 2011',degree:'GCE Ordinary Level', place: "ST.Thomas' Boys' College Matara", result: 'A-4 B-2 C-3'}
 ]
